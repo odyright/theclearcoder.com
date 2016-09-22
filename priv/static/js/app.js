5b4ec8a749905ee8eff5f102be9754fefe02556e
Brian Gamble <cc@theclearcoder.com>
2016-09-22 14:51:30 -0500

    Add google analytics tracking

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1,3 +1,10 @@
-// for phoenix_html support, including form and button helpers
-// copy the following scripts into your javascript bundle:
-// * https://raw.githubusercontent.com/phoenixframework/phoenix_html/v2.3.0/priv/static/phoenix_html.js
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53847301-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
