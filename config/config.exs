d29b32e93b0df25cd050f9733ab50d5791fd8631
Brian Gamble <cc@theclearcoder.com>
2016-09-21 22:05:34 -0500

    Initial commit

---
+++ config/config.exs
@@ -0,0 +1,23 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Configures the endpoint
+config :blog, Blog.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "rMB57c4S2vu6d2qcpNb1KuiUrGyrzbRp9vPQur7ZUrRv9vpIS+sMtDK/Sr3aukUi",
+  render_errors: [view: Blog.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Blog.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
