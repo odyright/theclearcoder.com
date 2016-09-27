59fd6c038d87d97f624353233fe74119013c87d0
Brian Gamble <cc@theclearcoder.com>
2016-09-27 18:56:38 -0500

    Add sass to project and move css to styles.scss

--- brunch-config.js
+++ brunch-config.js
@@ -20,10 +20,7 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: "css/app.css",
-      order: {
-        after: ["web/static/css/app.css"] // concat app.css last
-      }
+      joinTo: "css/app.css"
     },
     templates: {
       joinTo: "js/app.js"
