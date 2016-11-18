b4a4635fe1a2fc53135457df4ede51705b1d0658
Brian Gamble <cc@theclearcoder.com>
2016-11-18 17:22:44 -0600

    Add podcast show page

--- web/controllers/podcast_controller.ex
+++ web/controllers/podcast_controller.ex
@@ -8,4 +8,19 @@ defmodule Blog.PodcastController do
     podcasts = Repo.all(query)
     render conn, "index.html", podcasts: podcasts
   end
+
+  def show(conn, %{"id" => id}) do
+    podcast = Repo.get_by(Podcast, episode: id)
+    render_podcast(conn, podcast)
+  end
+
+  defp render_podcast(conn, nil) do
+    conn
+    |> put_status(:not_found)
+    |> render(Blog.ErrorView, "404.html")
+  end
+
+  defp render_podcast(conn, podcast) do
+    render conn, "show.html", podcast: podcast
+  end
 end
