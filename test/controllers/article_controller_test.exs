24ce1be80ca21f52480f17f2c3a59c840823f5e1
Brian Gamble <cc@theclearcoder.com>
2016-12-03 11:11:55 -0600

    Add tests to verify authentication required for some article actions

--- test/controllers/article_controller_test.exs
+++ test/controllers/article_controller_test.exs
@@ -1,6 +1,19 @@
 defmodule Blog.ArticleControllerTest do
   use Blog.ConnCase
 
+  test "authentication required for new, create, edit, update, delete", %{conn: conn} do
+    Enum.each [
+      get(conn, article_path(conn, :new)),
+      post(conn, article_path(conn, :create, %{})),
+      get(conn, article_path(conn, :edit, "1")),
+      put(conn, article_path(conn, :update, "1", %{})),
+      delete(conn, article_path(conn, :delete, "1"))
+    ], fn conn ->
+      assert html_response(conn, 302)
+      assert conn.halted
+    end
+  end
+
   test "GET /articles", %{conn: conn} do
     conn = get conn, "/articles"
     assert html_response(conn, 200) =~ "<title>Articles | Brian Gamble</title>"
