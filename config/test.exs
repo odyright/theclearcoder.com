d148370a74358636f3011b8d31de5581814b9893
Brian Gamble <cc@theclearcoder.com>
2016-12-04 09:03:25 -0600

    Add config to speed up tests

--- config/test.exs
+++ config/test.exs
@@ -17,3 +17,7 @@ config :blog, Blog.Repo,
   database: "blog_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+# add the following config to speed up tests
+config :comeonin, :bcrypt_log_rounds, 4 
+config :comeonin, :pbkdf2_rounds, 1
