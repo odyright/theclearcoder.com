655b8177b825d87276fac4892315cfc457fe86a3
Brian Gamble <cc@theclearcoder.com>
2016-12-03 20:01:10 -0600

    Add tests for create in UserController

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -1,5 +1,5 @@
 defmodule Blog.UserControllerTest do
-  use Blog.ConnCase
+  use Blog.ConnCase, async: true
 
   describe "guest access" do
     test "requires login for all user actions", %{conn: conn} do
@@ -35,11 +35,28 @@ defmodule Blog.UserControllerTest do
       assert html_response(conn, 200) =~ ~r/New User/
       assert String.contains?(conn.resp_body, "form")
     end
+
+    test "create/2 redirects to the index page", %{conn: conn} do
+      conn = post conn, user_path(conn, :create, %{"user" => new_user()})
+      assert html_response(conn, 302)
+      assert conn.request_path == user_path(conn, :index) 
+    end
+
+    test "create/2 save a new user to the database", %{conn: conn} do
+      user = new_user()
+      post conn, user_path(conn, :create, %{"user" => user})
+      saved_user = Repo.get_by(Blog.User, username: user["username"])
+      assert user["name"] == saved_user.name
+    end
   end
 
   defp login_test_user(context) do
     user = Fixtures.create(:user)
     conn = assign(context[:conn], :current_user, user)
     [conn: conn, user: user]
+  end
+
+  defp new_user() do
+    %{"name" => "John Doe", "password" => "password", "username" => "jdoe"}
   end
 end
