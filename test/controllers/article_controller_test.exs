cfe03a4891cf5a62606e80d4d7103d3a710f23e6
Brian Gamble <cc@theclearcoder.com>
2016-12-09 15:38:35 -0600

    Rework a couple of tests to get more specific

--- test/controllers/article_controller_test.exs
+++ test/controllers/article_controller_test.exs
@@ -16,14 +16,14 @@ defmodule Blog.ArticleControllerTest do
 
   test "GET /articles", %{conn: conn} do
     conn = get conn, "/articles"
-    assert html_response(conn, 200) =~ "<title>Articles | Brian Gamble</title>"
+    assert html_response(conn, 200) =~ "Articles"
   end
 
   test "GET /articles/:slug for a known article", %{conn: conn} do
-    Fixtures.create(:article)
+    article = Fixtures.create(:article)
 
     conn = get conn, "/articles/test-article"
-    assert html_response(conn, 200) =~ "<title>Test Article | Brian Gamble</title>"
+    assert html_response(conn, 200) =~ article.title
   end
 
   test "GET /articles/:slug for a bad article", %{conn: conn} do
