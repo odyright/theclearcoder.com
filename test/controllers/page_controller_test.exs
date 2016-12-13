dc49d85885c244ee7bffc9aab8a710c53b78f325
Brian Gamble <cc@theclearcoder.com>
2016-12-13 17:14:31 -0600

    Refactor tests for improved accuracy and readability

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,30 +1,37 @@
 defmodule Blog.PageControllerTest do
   use Blog.ConnCase, async: true
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "<title>Welcome | Brian Gamble</title>"
+  test "'index' renders the homepage", %{conn: conn} do
+    conn = get conn, page_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Welcome/
   end
 
-  test "GET /info", %{conn: conn} do
-    conn = get conn, "/info"
-    assert html_response(conn, 200) =~ "<title>Info | Brian Gamble</title>"
+  test "'info' renders the info page", %{conn: conn} do
+    conn = get conn, page_path(conn, :info)
+    assert html_response(conn, 200) =~ ~r/Info/
   end
 
-  test "GET /projects", %{conn: conn} do
-    conn = get conn, "/projects"
-    assert html_response(conn, 200) =~ "The Coding Show"
+  test "'projects' renders the project page", %{conn: conn} do
+    conn = get conn, page_path(conn, :projects)
+    assert html_response(conn, 200) =~ ~r/Projects/
   end
 
-  test "when let's encrypt is off it returns nothing", %{conn: conn} do
-    conn = get conn, "/.well-known/acme-challenge/aabbccddeeff"
-    assert text_response(conn, 200) == ""
+  test "'keybase' renders the keybase info page", %{conn: conn} do
+    conn = get conn, page_path(conn, :keybase)
+    assert text_response(conn, 200) =~ ~r/keybase.io/
   end
 
-  test "when let's encrypt is on it returns a response", %{conn: conn} do
-    conn = assign(conn, :letsencrypt, "ON")
-    conn = assign(conn, :letsencrypt_response, "bar")
-    conn = get conn, "/.well-known/acme-challenge/aabbccddeeff"
-    assert text_response(conn, 200) == "aabbccddeeffbar"
+  describe "let's encrypt" do
+    test "does nothing when off", %{conn: conn} do
+      conn = get conn, page_path(conn, :letsencrypt, "aabbccddeeff")
+      assert text_response(conn, 200) == ""
+    end
+
+    test "returns a response when on", %{conn: conn} do
+      conn = assign(conn, :letsencrypt, "ON")
+      conn = assign(conn, :letsencrypt_response, "bar")
+      conn = get conn, page_path(conn, :letsencrypt, "aabbccddeeff")
+      assert text_response(conn, 200) == "aabbccddeeffbar"
+    end
   end
 end
