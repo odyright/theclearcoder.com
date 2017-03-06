890b6fd00e321ec1f11066795343920430b5f98d
Brian Gamble <cc@theclearcoder.com>
2017-03-06 13:26:28 -0600

    Update seeds.exs: add articles and clear out existing data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,3 +1,7 @@
+# Clear out existing data
+Blog.Repo.delete_all(Blog.Branding)
+Blog.Repo.delete_all(Blog.Article)
+
 # Add dummy branding for dev
 Forge.saved_branding(
   item: "headline", 
@@ -6,3 +10,9 @@ Forge.saved_branding(
 Forge.saved_branding(item: "intro", copy: Enum.join(Faker.Lorem.paragraphs(2..4), "\n\n"))
 Forge.saved_branding(item: "bio", copy: Enum.join(Faker.Lorem.sentences(3..5), "\n"))
 Forge.saved_branding(item: "colophon", copy: Enum.join(Faker.Lorem.sentences(2..4), "\n"))
+
+# Add 15 dummy articles from the past 90 days
+days = Enum.map(1..15, fn(_) -> Enum.random(1..90) end)
+for n <- days do
+  Forge.saved_article(inserted_at: Timepiece.days_ago(n))
+end
