41b2b4152732dcd3eff82c4c160c0fed6cb0f259
Brian Gamble <cc@theclearcoder.com>
2017-03-12 04:10:29 -0500

    Refactor article form

--- test/views/article_view_test.exs
+++ test/views/article_view_test.exs
@@ -1,6 +1,7 @@
 defmodule Blog.ArticleViewTest do
-  use Blog.ConnCase
-  alias Blog.ArticleView
+  use Blog.ConnCase, async: true
+  import Phoenix.View
+  alias Blog.{ArticleView, Article}
   
   test "returns the page title for the index page" do
     assert ArticleView.page_title(:index, %{}) == "Articles"
@@ -9,5 +10,26 @@ defmodule Blog.ArticleViewTest do
   test "returns the page title for the show page" do
     article = Forge.article
     assert ArticleView.page_title(:show, %{article: article}) == article.title
+  end
+
+  test "render new.html", %{conn: conn} do
+    content = render_to_string(ArticleView, "new.html", 
+                               conn: conn, changeset: Article.changeset(%Article{})) 
+    assert content =~ "form"
+    assert content =~ "name=\"article[title]\""
+    assert content =~ "name=\"article[teaser]\""
+    assert content =~ "name=\"article[content]\""
+    assert content =~ "Create Article"
+  end
+
+  test "render edit.html", %{conn: conn} do
+    article = Forge.saved_article
+    content = render_to_string(ArticleView, "edit.html",
+                               conn: conn, changeset: Article.changeset(article))
+    assert content =~ "form"
+    assert content =~ "value=\"#{article.title}\""
+    assert content =~ "value=\"#{article.teaser}\""
+    assert content =~ article.content
+    assert content =~ "Update Article"
   end
 end
