d2341249939586f0b7337fa5bb8a8c06f396d4cf
Brian Gamble <cc@theclearcoder.com>
2017-04-04 11:31:19 -0500

    Update prod config and endpoint based on the Phoenix 1.3 changes

--- lib/blog/web/endpoint.ex
+++ lib/blog/web/endpoint.ex
@@ -39,4 +39,16 @@ defmodule Blog.Web.Endpoint do
     signing_salt: System.get_env("SIGNING_SALT") || "r3zeRM7N"
 
   plug Blog.Web.Router
+
+  @doc """
+  Dynamically loads configuration from the system environment
+  on startup.
+
+  It receives the endpoint configuration from the config files
+  and must return the updated configuration.
+  """
+  def load_from_system_env(config) do
+    port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
+    {:ok, Keyword.put(config, :http, [:inet6, port: port])}
+  end
 end
